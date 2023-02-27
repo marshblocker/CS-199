067481f865c642c1462d73d3eb4baa421cbf942e
Kenneth <gabrielkennethmarinas@gmail.com>
2023-02-27 10:59:08 +0800

    Update

--- main.py
+++ main.py
@@ -5,17 +5,9 @@ import numpy as np
 import os
 
 K = 10
-NU = 0.01
-FLOAT_RANGE = 10
+NU = 0.06
 
-print('NU = {}'.format(NU))
-
-TMAX_DEFAULT = 32
-TMIN_DEFAULT = 24
-TMEAN_DEFAULT = 28
-RH_DEFAULT = 75
-
-metro_manila = ['NAIA','Port Area','Sangley Point']
+metro_manila = ['NAIA','Port Area','Sangley Point', 'Science Garden']
 
 def get_data(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
@@ -23,10 +15,15 @@ def get_data(path: str) -> pd.DataFrame:
     df.sort_values(by=['YEAR', 'MONTH', 'DAY'])
     df = df[['YEAR', 'MONTH', 'DAY', 'TMAX', 'TMIN', 'TMEAN', 'RH']]
 
-    df.loc[df['TMAX'] == -999, 'TMAX'] = TMAX_DEFAULT
-    df.loc[df['TMIN'] == -999, 'TMIN'] = TMIN_DEFAULT
-    df.loc[df['TMEAN'] == -999, 'TMEAN'] = TMEAN_DEFAULT
-    df.loc[df['RH'] == -999, 'RH'] = RH_DEFAULT
+    tmax_mean = df[df['TMAX'] != -999]['TMAX'].mean()
+    tmin_mean = df[df['TMIN'] != -999]['TMIN'].mean()
+    tmean_mean = df[df['TMEAN'] != -999]['TMEAN'].mean()
+    rh_mean = df[df['RH'] != -999]['RH'].mean()
+
+    df.loc[df['TMAX'] == -999, 'TMAX'] = tmax_mean
+    df.loc[df['TMIN'] == -999, 'TMIN'] = tmin_mean
+    df.loc[df['TMEAN'] == -999, 'TMEAN'] = tmean_mean
+    df.loc[df['RH'] == -999, 'RH'] = rh_mean
 
     return df
 
@@ -67,7 +64,7 @@ def main():
             }
             stations[filename.rstrip('.csv')] = df
 
-    year = 2011
+    year = 2016
     month = 1
 
     for i in range(1, 12):
@@ -95,9 +92,6 @@ def main():
             return_estimator=True
         )
 
-        # print('test_score:')
-        # print(cv_res['test_score'])
-
         estimators = cv_res['estimator']
 
         X_test_clean = stations['NAIA'][year+1]
@@ -106,7 +100,7 @@ def main():
 
         y_test_clean = np.full((len(X_test_clean), 1), 1)
 
-        X_test_mal = stations['Baguio'][year]
+        X_test_mal = stations['Baguio'][year+1]
         X_test_mal = X_test_mal[X_test_mal['MONTH'] == month]
         X_test_mal = X_test_mal[['TMAX', 'TMIN', 'TMEAN', 'RH']].to_numpy()
 
@@ -114,8 +108,6 @@ def main():
 
         X_test = np.concatenate((X_test_clean, X_test_mal))
         y_test = np.concatenate((y_test_clean, y_test_mal))
-
-        # print(X_test_clean.shape, X_test_mal.shape, y_test_clean.shape, y_test_mal.shape, X_test.shape, y_test.shape)
 
         ave_f_score = round(sum(map(
             lambda clf: test(X_test, y_test, clf),
