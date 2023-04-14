7bbe3d2e71f9c098696092f7cd855fd228c18f7b
Kenneth <gabrielkennethmarinas@gmail.com>
2023-04-14 17:39:34 +0800

    Automate manual decision in SRP; refactor SRP

--- simulation/simulations/classes/srp.py
+++ simulation/simulations/classes/srp.py
@@ -1,7 +1,17 @@
+from copy import deepcopy
 from datetime import datetime
+from enum import Enum, auto
 
 MAX_TRUST_POINTS = 30
 M = 5
+
+
+class SRPEvalResult(Enum):
+    Normal = auto()
+    HackedSensors = auto()
+    LegitimateReadingShift = auto()
+    Unreachable = auto()
+
 
 class SensorRetentionPolicy:
     def __init__(self, initial_trust_points: int):
@@ -33,83 +43,85 @@ class SensorRetentionPolicy:
         self.K -= 1
         self.theta = self.K
 
-    def evaluate_sensors(self, classification_res, curr_date: datetime):
-        print(self.sensors_stats)
+    def evaluate_sensors(self, classification_result, curr_date: datetime):
+        # We need to deepcopy this to not mutate the original object
+        classif_result = deepcopy(classification_result)
+        classif_result = {sensor_id: [classif_result[sensor_id][0],
+                                      classif_result[sensor_id][1]] for sensor_id in classif_result}
+
+        # Convert -1 to True and 1 to False
+        for sensor_id in classif_result:
+            classif_result[sensor_id][0] = classif_result[sensor_id][0] == -1
+            classif_result[sensor_id][1] = classif_result[sensor_id][1] == -1
+
         self.curr_date = curr_date
-        malicious_amount = sum([
-            1 for is_malicious in list(classification_res.values()) if is_malicious
-        ])
+        malicious_amount = sum(
+            [res[0] for res in list(classif_result.values())])
 
         if self.K > 1 and malicious_amount >= self.theta:
-            return self._do_manual_investigation()
+            result = self._do_manual_investigation(classif_result)
+            print('sensor stats: {}'.format(self.sensors_stats))
+            return result
         else:
-            return self._update_trust_points(classification_res)
+            classif_result = {
+                sensor_id: classif_result[sensor_id][0] for sensor_id in classif_result}
+            result = self._update_trust_points(classif_result)
+            print('sensor stats: {}'.format(self.sensors_stats))
+            return result
 
-    def _do_manual_investigation(self):
+    def _do_manual_investigation(self, classification_result):
+        # For simulation purposes, if a classification result is correct, then
+        # the result of the manual investigation is hack, otherwise its
+        # legitimate reading shift
+        hacked_sensors = self._get_hacked_sensors(classification_result)
+
+        if hacked_sensors:
+            # Remove hacked sensor in list of monitored sensors.
+            # The removed sensor will also be removed in self.sensor_stats.
+            for sensor_id in hacked_sensors:
+                self.unregister_sensor(sensor_id)
+
+            # Possibly increase trust points of non-hacked sensors.
+            for sensor_id in self.sensors_stats:
+                self._is_not_malicious_action(sensor_id)
+
+            return (hacked_sensors, SRPEvalResult.HackedSensors)
+        else:
+            # There is a legitimate reading shift.
+            return ([], SRPEvalResult.LegitimateReadingShift)
+
+    # Returns a list of hacked sensors. If the list is empty, then
+    # no sensors were hacked so the decision must be legitimate
+    # reading shift.
+    def _get_hacked_sensors(self, classification_result):
+        hacked_sensors = []
+        for sensor_id in classification_result:
+            result = classification_result[sensor_id][0]
+            label = classification_result[sensor_id][1]
+            if result and label:
+                hacked_sensors[sensor_id]
+
+        return hacked_sensors
+
+    def _update_trust_points(self, classification_result):
         removed_sensors = []
-        print('Commence manual investigation...')
-        investigation_res = ''
-
-        while True:
-            investigation_res = input(
-                "Result of manual investigation ('s' for legitimate spike/dip in sensor readings and 'h' for hacking of sensors): ")
-            if investigation_res.lower() in ['s', 'h']:
-                investigation_res = investigation_res.lower()
-                break
-
-        if investigation_res == 's':
-            message_back = 'Retrain classifier from scratch'
-            return (removed_sensors, message_back)
-        elif investigation_res == 'h':
-            invalid_input = True
-            while invalid_input:
-                hacked_sensors = input(
-                    'List the ids of all the hacked sensors (format: <sensor_id_1>,<sensorid2>,...): ')
-                hacked_sensors = hacked_sensors.split(',')
-
-                sensors = list(self.sensors_stats.keys())
-                for sensor in hacked_sensors:
-                    if sensor not in sensors:
-                        print('{} is not in the list of sensors.'.format(sensor))
-                        break
-                else:
-                    for sensor_id in hacked_sensors:
-                        self.unregister_sensor(sensor_id)
-                        removed_sensors.append(sensor_id)
-
-                        print("{}: '{}' is removed from sensors since it was hacked!".format(
-                            self.curr_date, sensor_id))
-
-                    for sensor_id in self.sensors_stats:
-                        self._is_not_malicious_action(sensor_id)
-
-                    message_back = 'Hacked sensors'
-                    return (removed_sensors, message_back)
-        return ([], 'Cannot be reached')
-
-    def _update_trust_points(self, classification_res):
-        removed_sensors = []
-        for sensor_id, is_malicious in classification_res.items():
+        for sensor_id, is_malicious in classification_result.items():
             if is_malicious:
                 self._is_malicious_action(sensor_id, removed_sensors)
             else:
                 self._is_not_malicious_action(sensor_id)
 
-        message_back = 'Updated trust points'
-        return (removed_sensors, message_back)
+        return (removed_sensors, SRPEvalResult.Normal)
 
     def _is_malicious_action(self, sensor_id, removed_sensors):
         self.sensors_stats[sensor_id]['consecutive_clean'] = 0
         self.sensors_stats[sensor_id]['trust_points'] -= 1
 
-        if self.sensors_stats[sensor_id]['trust_points'] == 0:
+        if not self.sensors_stats[sensor_id]['trust_points']:
             self.unregister_sensor(sensor_id)
             removed_sensors.append(sensor_id)
             self.K -= 1
             self.theta = self.K
-
-            print("{}: '{}' is removed from the cluster since its trust points have reached 0!".format(
-                self.curr_date, sensor_id))
 
     def _is_not_malicious_action(self, sensor_id):
         self.sensors_stats[sensor_id]['consecutive_clean'] += 1
