19a70f07439a3abbf10f90df43e6b34bb1ad4fdd
Kenneth <gabrielkennethmarinas@gmail.com>
2023-04-14 17:40:09 +0800

    Add logs to Classifier class

--- simulation/simulations/classes/classifier.py
+++ simulation/simulations/classes/classifier.py
@@ -27,13 +27,15 @@ class Classifier:
             return_train_score=False,
             return_estimator=True
         )
-        print(cv_res['test_score'])
+        print('cv_res test_score: {}'.format(cv_res['test_score']))
 
         test_scores = list(cv_res['test_score'])
         max_score_indx = test_scores.index(max(test_scores))
         models = list(cv_res['estimator'])
         best_model = models[max_score_indx]
         self.models[month-1] = best_model
+
+        print('models: {}'.format(self.models))
 
     def classify(self, data: np.ndarray, month: int) -> np.ndarray:
         model = self.models[month-1]
