992d6bd45e0eb2605de038a831f537cc6ceb4958
Kenneth <gabrielkennethmarinas@gmail.com>
2023-04-17 09:30:24 +0800

    Update run.sh

--- simulation/run.sh
+++ simulation/run.sh
@@ -12,21 +12,23 @@ fi
 ip_addr="$1"
 http_port="$2"
 
-for i in {1..100}
+
+x=1
+for i in {001..100}
 do
      rm -rf ./geth/keystore ./geth/geth ./geth/genesis.json &&
-     echo "[$i] Finished removing geth artifacts from previous run."
+     echo "[$x] Finished removing geth artifacts from previous run."
 
      geth --verbosity 1 account new --password ./geth/password.txt --datadir ./geth > /dev/null &&
-     echo "[$i] Finished creating new account."
+     echo "[$x] Finished creating new account."
 
      account_address=`python3.10 ./geth/scripts/build-genesis.py` &&
-     echo "[$i] Finished creating genesis.json."
+     echo "[$x] Finished creating genesis.json."
 
      geth --verbosity 1 init --datadir geth ./geth/genesis.json > /dev/null &&
-     echo "[$i] Finished initializing blockchain."
+     echo "[$x] Finished initializing blockchain."
 
-     echo "[$i] Running geth client..."
+     echo "[$x] Running geth client..."
      geth --datadir ./geth \
           --nat extip:$ip_addr \
           --networkid 123 \
@@ -44,26 +46,26 @@ do
           --verbosity 1 &
 
      s=10
-     echo "[$i] Sleep for $s seconds to wait for geth to stabilize..."
+     echo "[$x] Sleep for $s seconds to wait for geth to stabilize..."
      sleep $s
-     echo "[$i] Finished sleeping."
+     echo "[$x] Finished sleeping."
 
      contract_address=`python3.10 ./geth/scripts/deploy-contract.py $http_port` &&
-     echo "[$i] Finished deploying contract into the blockchain."
+     echo "[$x] Finished deploying contract into the blockchain."
 
      sys_type="$3"
 
      if [ "$sys_type" == "with-srp" ]; then
           itp="$4"
-          echo "[$i] Running test suite for '$sys_type-itp-$itp' system variation..."
-          python3.10 ./simulations/run-test-suite.py $sys_type $http_port $contract_address $itp $i > "./simulations/logs/with-srp-$itp/test-case-$i-logs.txt"
+          echo "[$x] Running test suite for '$sys_type-itp-$itp' system variation..."
+          python3.10 ./simulations/run-test-suite.py $sys_type $http_port $contract_address $itp $x > "./simulations/logs/with-srp-$itp/test-case-$i-logs.txt"
      elif [ "$sys_type" == "without-srp" ]; then
-          echo "[Test Case $i] Running test suite for '$sys_type' system variation..."
-          python3.10 ./simulations/run-test-suite.py $sys_type $http_port $contract_address $i > "./simulations/logs/without-srp/test-case-$i-logs.txt"
+          echo "[$x] Running test suite for '$sys_type' system variation..."
+          python3.10 ./simulations/run-test-suite.py $sys_type $http_port $contract_address $x > "./simulations/logs/without-srp/test-case-$i-logs.txt"
 
      elif [ "$sys_type" == "without-mndp" ]; then
-          echo "[$i] Running test suite for '$sys_type' system variation..."
-          python3.10 ./simulations/run-test-suite.py $sys_type $i > "./simulations/logs/without-mndp/test-case-$i-logs.txt"
+          echo "[$x] Running test suite for '$sys_type' system variation..."
+          python3.10 ./simulations/run-test-suite.py $sys_type $x > "./simulations/logs/without-mndp/test-case-$i-logs.txt"
      else
           echo "Error: Invalid system type!"
           exit 1
@@ -71,13 +73,13 @@ do
 
      pid=`ps | grep geth | sed 's@^[^0-9]*\([0-9]\+\).*@\1@'`
      kill $pid &&
-     echo "[$i] Finished killing geth process"
+     echo "[$x] Finished killing geth process"
 
-     echo "[$i] Sleep for $s seconds before running next test case..."
+     echo "[$x] Sleep for $s seconds before running next test case..."
      sleep $s
-     echo "[$i] Finished sleeping"
+     echo "[$x] Finished sleeping"
 
-     echo "[$i] Test done!"
+     echo "[$x] Test done!"
      python3.10 ./simulations/utils/ring.py
 done
 
