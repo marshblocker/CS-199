a6132cf0a8f6d6f9c8a7f0f52d88462b261fae32
Kenneth <gabrielkennethmarinas@gmail.com>
2023-04-30 19:36:43 +0800

    Do not recompute malicious data every test case run

--- simulation/simulations/classes/sensor.py
+++ simulation/simulations/classes/sensor.py
@@ -4,10 +4,12 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
 
 class Sensor:
-    PAGASA_PATH = os.path.join(os.path.dirname(
-        __file__), '..', '..', '..', 'PAGASA')
+    PAGASA_PATH = os.path.join(DIR_PATH, '..', '..', '..', 'PAGASA')
+    MALICIOUS_DATA_PATH = os.path.join(DIR_PATH, '..', '..', 'malicious-data')
     # This is needed since Ethereum does not have float type.
     PRECISION = 100
 
@@ -73,30 +75,7 @@ class Sensor:
         return df
 
     def _get_malicious_data(self):
-        new_rows = []
-        for (year, month), group in self.data.groupby(['YEAR', 'MONTH']):
-            tmax_max = group['TMAX'].max()
-            tmax_min = group['TMAX'].min()
-            tmin_max = group['TMIN'].max()
-            tmin_min = group['TMIN'].min()
-            tmean_max = group['TMEAN'].max()
-            tmean_min = group['TMEAN'].min()
-            rh_max = group['RH'].max()
-            rh_min = group['RH'].min()
-            wind_speed_max = group['WIND_SPEED'].max()
-            wind_speed_min = group['WIND_SPEED'].min()
-            delta = np.random.randint(1, 51)
-            for _, row in group.iterrows():
-                new_tmax_val = np.random.choice(
-                    [tmax_max, tmax_min]) + delta * np.random.choice([-1, 1])
-                new_tmin_val = np.random.choice(
-                    [tmin_max, tmin_min]) + delta * np.random.choice([-1, 1])
-                new_tmean_val = np.random.choice(
-                    [tmean_max, tmean_min]) + delta * np.random.choice([-1, 1])
-                new_rh_val = np.random.choice(
-                    [rh_max, rh_min]) + delta * np.random.choice([-1, 1])
-                new_wind_speed_val = np.random.choice(
-                    [wind_speed_max, wind_speed_min]) + delta * np.random.choice([-1, 1])
-                new_rows.append([year, month, row['DAY'], new_tmax_val,
-                                new_tmin_val, new_tmean_val, new_rh_val, new_wind_speed_val])
-        return pd.DataFrame(new_rows, columns=['YEAR', 'MONTH', 'DAY', 'TMAX', 'TMIN', 'TMEAN', 'RH', 'WIND_SPEED'])
+        df = pd.read_csv(os.path.join(self.MALICIOUS_DATA_PATH,
+                         self.id.replace(' ', '-') + '-malicious.csv'))
+        print(df)
+        return df
